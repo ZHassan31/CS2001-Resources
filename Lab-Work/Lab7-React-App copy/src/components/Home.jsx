8358c6cf64114adcef5428042f68210fa4e3ae27
ZHassan31 <zubayr141@hotmail.com>
2024-12-04 10:24:39 +0000

    controller commit

---
+++ Lab-Work/Lab7-React-App copy/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import useFetchData from "../customHooks/useFetchData";
+
+export default function Home() {
+    const { data, error, loading } = useFetchData('http://localhost:8080/hello')
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+    // console.log(data)
+    // console.log(error)
+    return (
+        <div>
+            <p>{data}</p>
+        </div>
+
+    )
+
+}
